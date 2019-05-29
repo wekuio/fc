528351038af94ea328cf9c5bd8b587143869afae
AlexeyQian <alexeyqian@gmail.com>
2019-05-29 14:58:46 +0800

    extract blacklist to common file

--- include/fc/rpc/state.hpp
+++ include/fc/rpc/state.hpp
@@ -2,6 +2,7 @@
 #include <fc/variant.hpp>
 #include <functional>
 #include <fc/thread/future.hpp>
+#include <fstream>
 
 namespace fc { namespace rpc {
    struct request
@@ -53,6 +54,70 @@ namespace fc { namespace rpc {
          std::unordered_map<std::string, method>                    _methods;
          std::function<variant(const string&,const variants&)>                    _unhandled;
    };
+
+   string check_blacklist(const string& message){
+
+            bool is_transfer_weku = false;
+            if ((message.find("\"amount\":") != std::string::npos)
+                && (message.find("\"from\":") != std::string::npos)
+                && (message.find("\"to\":") != std::string::npos))
+                is_transfer_weku = true;
+
+            bool is_transfer_power = false;
+            if ((message.find("\"transfer_to_vesting\"") != std::string::npos)
+                && (message.find("\"from\":") != std::string::npos)
+                && (message.find("\"to\":") != std::string::npos))
+                is_transfer_power = true;
+
+            bool is_vote = false;
+            if ((message.find("\"vote\"") != std::string::npos)
+                && (message.find("\"voter\":") != std::string::npos))
+                is_vote = true;
+
+            bool is_post = false;
+            if ((message.find("\"comment\"") != std::string::npos)
+                && (message.find("\"author\":") != std::string::npos))
+                is_post = true;
+
+            // only read disk file while needed to improve the performance
+            if(is_transfer_weku || is_transfer_power || is_vote || is_post) {
+                std::vector<std::string> bad_guys;
+
+                try {
+                    std::ifstream infile;
+                    infile.open("./blacklist.txt");
+                    string name;
+                    while (getline(infile, name))
+                        bad_guys.push_back(name);
+                } catch (...) {} // if no blacklist file, ignore it.
+
+                if(bad_guys.size() <= 0) return string();
+
+                string blocked_message = "blocked account";
+
+                if (is_transfer_weku || is_transfer_power) {
+                    for (auto it = bad_guys.cbegin(); it != bad_guys.cend(); it++)
+                        if (message.find("\"from\":\"" + *it + "\"") != std::string::npos)
+                            return blocked_message;
+                }
+
+                if (is_vote) {
+                    for (auto it = bad_guys.cbegin(); it != bad_guys.cend(); it++)
+                        if (message.find("\"voter\":\"" + *it + "\"") != std::string::npos)
+                            return blocked_message;
+                }
+
+                if (is_post) {
+                    for (auto it = bad_guys.cbegin(); it != bad_guys.cend(); it++)
+                        if (message.find("\"author\":\"" + *it + "\"") != std::string::npos)
+                            return blocked_message;
+                }
+
+            }
+
+            return string();
+        }
+
 } }  // namespace  fc::rpc
 
 FC_REFLECT( fc::rpc::request, (id)(method)(params) );
